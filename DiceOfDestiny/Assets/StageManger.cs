f503d5fbf932ddac4bd69fdcc8cc18fc751e8b23
op4804 <op4804@gmail.com>
2025-07-02 18:13:10 +0900

    init

---
+++ DiceOfDestiny/Assets/StageManger.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class StageManger : MonoBehaviour
+{
+    void SetBoard()
+    {
+
+    }    
+}
---
+++ DiceOfDestiny/Assets/Tile.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    string tileColor;
+    string obstacle;
+}
