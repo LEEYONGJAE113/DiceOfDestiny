e747f8ecf72245ee2de41c76242213abf6f3c098
Accdo <khyeong1002@gmail.com>
2025-07-04 18:11:50 +0900

    feat : HistoryManager -ing

--- DiceOfDestiny/Assets/GH_Folder/GameManager.cs
+++ DiceOfDestiny/Assets/GH_Folder/GameManager.cs
@@ -4,8 +4,11 @@ public class GameManager : Singletone<GameManager>
 {
     public ActionPointManager actionPointManager { get; private set; }
 
+    public HistoryManager historyManager { get; private set; }
+
     private void Awake()
     {
         actionPointManager = GetComponent<ActionPointManager>();
+        historyManager = GetComponent<HistoryManager>();
     }
 }
