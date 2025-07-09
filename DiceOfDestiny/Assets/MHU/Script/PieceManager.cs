ab923d542a1d045587e0156e629faa45a63ae467
LEEYONGJAE113 <ionsi1103@gmail.com>
2025-07-09 09:12:24 +0900

    피스매니저랑 ui매니저는 일단 완 아닐까요

--- DiceOfDestiny/Assets/MHU/Script/PieceManager.cs
+++ DiceOfDestiny/Assets/MHU/Script/PieceManager.cs
@@ -8,15 +8,14 @@ public class PieceManager : Singletone<PieceManager>
     public GameObject piecePrefab;
     private PieceController currentPiece; // 현재 내가 조종중인 말
 
-    // public void TurnStart()
-    // {
-    //     foreach (var item in pieces)
-    //     {
-    //         var uiController = item.gameObject.GetComponentInChildren<PieceUIManager>();
-    //         uiController?.ChangeUI(States.Selectable);
-    //     }
-    // }
+    public void DrawAllPieceUIs()
+    {
+        for (int i = 0; i < pieces.Count; i++)
+        {
+            PieceUIManager.Instance.CreateUI(pieceStates[i].CurrentState, pieces[i].gameObject.transform.position);
+        }
+    }
 
-    
+
 
 }
