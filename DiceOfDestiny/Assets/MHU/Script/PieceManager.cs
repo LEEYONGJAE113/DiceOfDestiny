12db616eb93aecd4aa8d393788652a3789f4cdc3
LEEYONGJAE113 <ionsi1103@gmail.com>
2025-07-09 09:35:16 +0900

    피스매니저 해치웟나

--- DiceOfDestiny/Assets/MHU/Script/PieceManager.cs
+++ DiceOfDestiny/Assets/MHU/Script/PieceManager.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 public class PieceManager : Singletone<PieceManager>
 {
     List<PieceController> pieces = new List<PieceController>();
+    public List<PieceController> Pieces
+    {
+        get => pieces;
+        set
+        {
+            pieces = value;
+            EventManager.Instance.TriggerEvent(AllEventNames.PIECE_COUNT_CHANGED);
+        }
+    }
     private List<PieceState> pieceStates = new();
     public GameObject piecePrefab;
     private PieceController currentPiece; // 현재 내가 조종중인 말
+
+    void Awake()
+    {
+        UpdatePieceManagerList();
+    }
+
+    void Start()
+    {
+        EventManager.Instance.AddListener(AllEventNames.PIECE_COUNT_CHANGED, UpdatePieceManagerList);
+    }
 
     public void DrawAllPieceUIs()
     {
@@ -16,6 +35,20 @@ public class PieceManager : Singletone<PieceManager>
         }
     }
 
-
+    private void UpdatePieceManagerList(object data = null)
+    {
+        int count = pieces.Count;
+        if (pieceStates.Count < count)
+        {
+            PieceState newElement = new PieceState();
+            newElement.ChangeState(States.Selectable);
+            newElement.ChangeSelectable(true);
+            pieceStates.Add(newElement);
+        }
+        if (pieceStates.Count > count)
+        {
+            pieceStates.RemoveAt(pieceStates.Count - 1);
+        }
+    }
 
 }
