5e09fae8a12ac6f98558398cd01ee7d91ed3869d
op4804 <op4804@gmail.com>
2025-07-24 16:16:50 +0900

    feat : dice customize - dice folding

--- DiceOfDestiny/Assets/CustomizingPiece.cs
+++ DiceOfDestiny/Assets/CustomizingPiece.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Drawing;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -15,6 +16,9 @@ public class CustomizingPiece : MonoBehaviour
     bool isFolding = false;
 
     [SerializeField] private float foldDuration = 1.0f;
+    [SerializeField] private float enlargeDuration = 1.0f;
+    [SerializeField] private float gatherDuration = 0.5f;
+    [SerializeField] private float space = 30;
 
 
     public void Start()
@@ -38,20 +42,164 @@ public class CustomizingPiece : MonoBehaviour
         else
         {
             FoldToDice();
+
         }
     }
-
 
     public void FoldToDice()
     {
         if (isFolding || isFolded)
             return;
+        isFolding = true;
+        StartCoroutine(GatherFaces());        
+    }
+
+    public void UnFoldToDiceNet()
+    {
+        if (isFolding || !isFolded)
+            return;
+        isFolding = true;
+        StartCoroutine(CompressOverTime());
+        StartCoroutine(MoveOverTime(true));
+    }
+
+    IEnumerator MoveOverTime(bool reverse = false)
+    {
+        float elapsedTime = 0f;
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        Vector3 initialPosition;
+        Vector3 targetPosition;
+
+        if (!reverse)
+        {
+            initialPosition = transform.localPosition;
+            targetPosition = initialPosition + Vector3.right * 550f;
+        }
+        else
+        {
+            initialPosition = transform.localPosition;
+            targetPosition = transform.localPosition + Vector3.left * 550f;
+        }
+
+        while (elapsedTime < enlargeDuration)
+        {
+            float t = elapsedTime / enlargeDuration;
+            transform.localPosition = Vector3.Lerp(initialPosition, targetPosition, t);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = targetPosition; // Ensure final position is set
+    }
+
+    IEnumerator EnlargeOverTime()
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < enlargeDuration)
+        {
+            float t = elapsedTime / enlargeDuration;
+            float scale = Mathf.Lerp(1f, 2f, t); // Scale from 1 to 2
+            transform.localScale = new Vector3(scale, scale, scale);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localScale = new Vector3(2f, 2f, 2f); // Ensure final scale is set
+
+        isFolded = true;
+        isFolding = false;
+    }
+
+    IEnumerator CompressOverTime()
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < enlargeDuration)
+        {
+            float t = elapsedTime / enlargeDuration;
+            float scale = Mathf.Lerp(2f, 1f, t); // Scale from 1 to 2
+            transform.localScale = new Vector3(scale, scale, scale);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localScale = new Vector3(1f, 1f, 1f); // Ensure final scale is set
+
+        StartCoroutine(UnFoldToDiceNetCoroutine());
+    }
+    
+    IEnumerator GatherFaces()
+    {
+        float elapsedTime = 0f;
+
+        float size = frontFace.GetComponent<RectTransform>().rect.width;
+
+        Vector3 backFacePosition = backFace.transform.position;
+        Vector3 leftFacePosition = leftFace.transform.position;
+        Vector3 rightFacePosition = rightFace.transform.position;
+        Vector3 topFacePosition = topFace.transform.position;
+        Vector3 bottomFacePosition = bottomFace.transform.position;
+
+        Vector3 backFaceTargetPosition = frontFace.transform.position + Vector3.right * size * 2f;
+        Vector3 leftFaceTargetPosition = frontFace.transform.position + Vector3.left * size;
+        Vector3 rightFaceTargetPosition = frontFace.transform.position + Vector3.right * size;
+        Vector3 topFaceTargetPosition = frontFace.transform.position + Vector3.up * size;
+        Vector3 bottomTargetFacePosition = frontFace.transform.position + Vector3.down * size;
+
+        while (elapsedTime < foldDuration)
+        {
+            float t = elapsedTime / foldDuration;
+
+            backFace.transform.position = Vector3.Lerp(backFacePosition, backFaceTargetPosition, t);
+            leftFace.transform.position = Vector3.Lerp(leftFacePosition, leftFaceTargetPosition, t);
+            rightFace.transform.position = Vector3.Lerp(rightFacePosition, rightFaceTargetPosition, t);
+            topFace.transform.position = Vector3.Lerp(topFacePosition, topFaceTargetPosition, t);
+            bottomFace.transform.position = Vector3.Lerp(bottomFacePosition, bottomTargetFacePosition, t);
+
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
         StartCoroutine(FoldToDiceCoroutine());
     }
 
+    IEnumerator ScatterFaces()
+    {
+        float elapsedTime = 0f;
+
+        float size = frontFace.GetComponent<RectTransform>().rect.width;
+
+        Vector3 backFacePosition = backFace.transform.position;
+        Vector3 leftFacePosition = leftFace.transform.position;
+        Vector3 rightFacePosition = rightFace.transform.position;
+        Vector3 topFacePosition = topFace.transform.position;
+        Vector3 bottomFacePosition = bottomFace.transform.position;
+
+        Vector3 backFaceTargetPosition = backFace.transform.position + Vector3.right * space * 2;
+        Vector3 leftFaceTargetPosition = leftFace.transform.position + Vector3.left * space;
+        Vector3 rightFaceTargetPosition = rightFace.transform.position + Vector3.right * space;
+        Vector3 topFaceTargetPosition = topFace.transform.position + Vector3.up * space;
+        Vector3 bottomTargetFacePosition = bottomFace.transform.position + Vector3.down * space;
+
+        while (elapsedTime < foldDuration)
+        {
+            float t = elapsedTime / foldDuration;
+
+            backFace.transform.position = Vector3.Lerp(backFacePosition, backFaceTargetPosition, t);
+            leftFace.transform.position = Vector3.Lerp(leftFacePosition, leftFaceTargetPosition, t);
+            rightFace.transform.position = Vector3.Lerp(rightFacePosition, rightFaceTargetPosition, t);
+            topFace.transform.position = Vector3.Lerp(topFacePosition, topFaceTargetPosition, t);
+            bottomFace.transform.position = Vector3.Lerp(bottomFacePosition, bottomTargetFacePosition, t);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        isFolding = false;
+        isFolded = false;
+    }
+
+
     IEnumerator FoldToDiceCoroutine()
     {
-        isFolding = true;
         float elapsedTime = 0f;
 
         float size = frontFace.GetComponent<RectTransform>().rect.width;
@@ -75,8 +223,6 @@ public class CustomizingPiece : MonoBehaviour
             float scale = Mathf.Lerp(1f, 0f, t);
             float backScale = Mathf.Lerp(1f, 0f, t * 2f); // Back face scales faster to hide it
 
-            backFace.transform.localScale = new Vector3(backScale, 1f, 1f);
-
             topFace.transform.localScale = new Vector3(1f, scale, 1f);
             bottomFace.transform.localScale = new Vector3(1f, scale, 1f);
             leftFace.transform.localScale = new Vector3(scale, 1f, 1f);
@@ -89,7 +235,7 @@ public class CustomizingPiece : MonoBehaviour
             bottomFace.transform.position = Vector3.Lerp(bottomFacePosition, bottomFaceTargetPosition, t);            
 
             elapsedTime += Time.deltaTime;
-            yield return null;
+            yield return null;           
         }
 
         backFace.transform.localScale = new Vector3(1f,0f,1f);
@@ -104,15 +250,8 @@ public class CustomizingPiece : MonoBehaviour
         topFace.transform.position = topFaceTargetPosition;
         bottomFace.transform.position = bottomFaceTargetPosition;
 
-        isFolded = true;
-        isFolding = false;
-    }
-
-    public void UnFoldToDiceNet()
-    {
-        if (isFolding || !isFolded)
-            return;
-        StartCoroutine(UnFoldToDiceNetCoroutine());
+        StartCoroutine(MoveOverTime());
+        StartCoroutine(EnlargeOverTime());
     }
 
     IEnumerator UnFoldToDiceNetCoroutine()
@@ -172,8 +311,7 @@ public class CustomizingPiece : MonoBehaviour
         topFace.transform.position = topFacePosition;
         bottomFace.transform.position = bottomFacePosition;
 
-        isFolded = false;
-        isFolding = false;
+        StartCoroutine(ScatterFaces());
     }
 
 }
