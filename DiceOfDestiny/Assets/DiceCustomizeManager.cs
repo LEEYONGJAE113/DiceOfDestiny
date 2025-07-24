e9cfc4d8e0393d140980a0a3df21d97e5113af13
op4804 <op4804@gmail.com>
2025-07-24 22:20:42 +0900

    feat : dice customize

--- DiceOfDestiny/Assets/DiceCustomizeManager.cs
+++ DiceOfDestiny/Assets/DiceCustomizeManager.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 public class DiceCustomizeManager : MonoBehaviour
 {
+    [SerializeField] private GameObject cutomizePanel;
+    [SerializeField] private GameObject carouselUIPanel;
+
     [SerializeField] private GameObject pieceCarouselUI;
     [SerializeField] private GameObject pieceNetCarouselUI;
+
+    [SerializeField] private GameObject piecesContent;
+    [SerializeField] private GameObject piecePreviewButtonPrefab;
+
+
+    public void InitializePiecesCaruselUI()
+    {
+
+    }
 
 
     public void OnClickPieceCaruselUIButton()
